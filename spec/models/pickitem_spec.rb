e9f6a15688017d65ba3a94bc4c86be539877e7fd
@chie-python <momochie0712@gmail.com>
2024-04-09 23:32:54 +0900

    edit pickitem model test

--- spec/models/pickitem_spec.rb
+++ spec/models/pickitem_spec.rb
@@ -1,12 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Pickitem, type: :model do
-  describe 'ピッキング実績登録' do
-    it 'shipperが空では登録できない' do
+  describe 'ピッキング実績更新' do
+    let(:pickitem){ Pickitem.create(shipper: "12345", shipping_records: 3)}
 
+    it 'shipperが入力されかつshipping_recordsが入力されれば更新できる' do
+      pickitem.update(shipper: "99999", shipping_records: 9)
+      expect(pickitem.shipper).to eq("99999")
+      expect(pickitem.shipping_records).to eq(9)
     end
-    it 'shipping_recordsが空では登録できない' do
-      
+    it 'shipperが空では更新できない' do
+      pickitem.update(shipper: nil)
+      expect(pickitem.errors[:shipper]).to include("can't be blank")
+    end
+    it 'shipping_recordsが空では更新できない' do
+      pickitem.update(shipping_records: nil)
+      expect(pickitem.errors[:shipping_records]).to include("can't be blank")
     end
   end
 end
